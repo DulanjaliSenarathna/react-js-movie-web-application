710d69f93d417bd9e7bf470a14368b344a51f642
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-04-05 22:55:39 +0530

    Tested API and fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
-import React,{ useState } from 'react'
+import React,{ useEffect} from 'react';
+import {fetchDataFromApi} from "./utils/api"
 
 function App() {
+
+  useEffect(()=>{
+    apiTesting();//invoke method
+  },[])//[]-dipendency
+
+const apiTesting = () =>{
+  fetchDataFromApi('/movie/popular?api_key=611f5b9b659c50118c4488a0dd035ece')
+    .then((res)=>{
+      console.log(res);
+    })
+}
 
   return (
     <div className="App">
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App'
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
     <App />
-  </React.StrictMode>,
+
 )
