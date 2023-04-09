2226d954b33bec553c67e20bf1d284e698916763
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-04-09 22:54:15 +0530

    Created common carousel and loaded trending movies and tv shows

---
+++ src/components/carousel/Carousel.jsx
@@ -0,0 +1,78 @@
+import React, { useRef } from "react";
+import {
+    BsFillArrowLeftCircleFill,
+    BsFillArrowRightCircleFill,
+} from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import dayjs from "dayjs";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import Img from "../lazyLoadImage/Img";
+import PosterFallback from "../../assets/no-poster.png";
+
+import "./style.scss";
+
+const Carousel = ({data,loading}) => {
+    const carouselContainer = useRef();
+    const {url} = useSelector((state)=> state.home)
+    const navigate = useNavigate()
+
+    const navigation = (dir)=>{
+
+    }
+
+    //skeleton item
+    const skItem = ()=>{
+        return (
+          <div className="skeletonItem">
+            <div className="posterBlock skeleton"></div>
+            <div className="textBlock">
+              <div className="title skeleton"></div>
+              <div className="date skeleton"></div>
+            </div>
+          </div>
+        )
+    }
+
+  return (
+    <div className="carousel">
+        <ContentWrapper>
+            <BsFillArrowLeftCircleFill className="carouselLeftNav arrow" onClick={()=> navigation("left")}/>
+            <BsFillArrowRightCircleFill className="carouselRighttNav arrow" onClick={()=> navigation("right")}/>
+            {!loading ? (
+              <div className="carouselItems">
+                {data?.map((item)=>{
+                  const posterUrl = item.poster_path ? url.poster + item.poster_path : PosterFallback
+                  return (
+                    <div className="carouselItem" key={item.id} >
+                      <div className="posterBlock">
+                        <Img src={posterUrl}/>
+                      </div>
+                      <div className="textBlock">
+                        <span className="title">
+                          {item.title || item.name}
+                        </span>
+                        <span className="date">
+                          {dayjs(item.release_date).format("MMM D, YYYY")}
+                        </span>
+                      </div>
+                    </div>
+                  )
+                })}
+              </div>
+            ) : (
+              <div className="loadingSkeleton">
+                {skItem()}
+                {skItem()}
+                {skItem()}
+                {skItem()}
+                {skItem()}
+              </div>
+            )}
+        </ContentWrapper>
+    </div>
+  )
+}
+
+export default Carousel
--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
 import SwitchTabs from '../../../components/switchTabs/SwitchTabs';
 import useFetch from './../../../hooks/useFetch';
+import Carousel from '../../../components/carousel/Carousel';
 
 const Trending = () => {
 
@@ -20,6 +21,7 @@ const onTabChange = (tab) =>{
             <span className="carouselTitle">Trending</span>
             <SwitchTabs data={["Day","Week"]} onTabChange={onTabChange}/>
         </ContentWrapper>
+        <Carousel data={data?.results} loading={loading}/>
     </div>
   )
 }
