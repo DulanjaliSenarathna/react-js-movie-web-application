bf74ff628de0ecd06823a8ffaa2bd89d4761499f
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-04-11 19:06:32 +0530

    Cast data fetched and displayed

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,6 +3,7 @@ import "./style.scss";
 import useFetch from './../../hooks/useFetch';
 import { useParams } from 'react-router-dom';
 import DetailsBanner from './detailsBanner/DetailsBanner';
+import Cast from './cast/Cast';
 
 const Details = () => {
 
@@ -13,6 +14,7 @@ const Details = () => {
   return (
     <div>
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew}/>
+      <Cast data={credits?.cast} loading={creditsLoading}/>
     </div>
   )
 }
