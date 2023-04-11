e6d51c4e216bfb49724282bae446fb5f482c3a45
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-04-11 14:45:54 +0530

    Details banner  created and fetched

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import "./style.scss"
+import "./style.scss";
+import useFetch from './../../hooks/useFetch';
+import { useParams } from 'react-router-dom';
+import DetailsBanner from './detailsBanner/DetailsBanner';
 
 const Details = () => {
+
+   const {mediaType, id} = useParams();
+   const {data, loading} = useFetch(`/${mediaType}/${id}`);
+
   return (
-    <div>Details</div>
+    <div>
+      <DetailsBanner/>
+    </div>
   )
 }
 
