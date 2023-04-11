6af3b9f71a339829ef7e7d1871188084139239c6
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-04-11 21:27:17 +0530

    Official videos section done with fetching data and loading data

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -4,6 +4,7 @@ import useFetch from './../../hooks/useFetch';
 import { useParams } from 'react-router-dom';
 import DetailsBanner from './detailsBanner/DetailsBanner';
 import Cast from './cast/Cast';
+import VideosSection from './videosSection/VideosSection';
 
 const Details = () => {
 
@@ -15,6 +16,7 @@ const Details = () => {
     <div>
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew}/>
       <Cast data={credits?.cast} loading={creditsLoading}/>
+      <VideosSection data={data} loading={loading}/>
     </div>
   )
 }
