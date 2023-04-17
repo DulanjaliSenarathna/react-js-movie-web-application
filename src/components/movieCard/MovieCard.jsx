646a8716548fe163e22443a3fe6d22671fb02fef
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-04-17 22:15:01 +0530

    Updated folder name

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import "./style.scss";
-import Img from "../lazyLoadImage/img";
+import Img from "../lazyLoadImage";
 import CircleRating from "../circleRating/CircleRating";
 import Genres from "../genres/Genres";
 import PosterFallback from "../../assets/no-poster.png";
